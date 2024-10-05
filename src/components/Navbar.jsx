7cacc26f6f7803044b47e390227ea868af4eda2d
Rajeev Krishna <radzhivkrishna@gmail.com>
2024-10-05 22:26:07 +0530

    feat. Navbar created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="p-3 border rounded-md">
+      <h1 className="text-2xl font-semibold">Chat Session</h1>
+    </div>
+  );
+};
+
+export default Navbar;
