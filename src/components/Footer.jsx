3a31b5103b5b971a36ffbd1784e5f21de0463af5
Rajeev Krishna <radzhivkrishna@gmail.com>
2024-10-05 22:34:02 +0530

    feat. Footer created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='mt-auto py-5'>
+        <p className='text-center'>Made with React and Tailwind</p>
+    </div>
+  )
+}
+
+export default Footer
