ea46de0357379ce96c8b6b4251b880c88e1eaa79
Rajeev Krishna <radzhivkrishna@gmail.com>
2024-10-05 22:36:26 +0530

    feat. Base UI created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
-
+import ChatSession from "./components/ChatSession"
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+import Sidebar from "./components/Sidebar"
+import { useState } from 'react'
 
 function App() {
+  const [selectedSession, setSelectedSession] = useState(null);
+
+  const handleSessionClick = (session) => {
+    setSelectedSession(session);
+  };
+
+  const closeSessionDetails = () => {
+    setSelectedSession(null);
+  };
+
   return (
-   <h1>Chat Session</h1>
+   <div className="p-5 flex flex-col h-full">
+    <Navbar />
+    <div className="my-2 p-2 flex md:flex-row flex-col gap-2 border rounded">
+    <Sidebar onSessionClick={handleSessionClick} />
+    <ChatSession session={selectedSession} onClose={closeSessionDetails} />
+    </div>
+    <Footer />
+   </div>
   )
 }
 
